489d43036baaabdfb019d3d25918004d706beddd
Eroscupid <vutuanduong2003@yahoo.com>
2021-09-22 10:19:00 -0600

    check sport event

--- sport_event_service/src/main/java/ca/nomosnow/sport_event_service/SportEventServiceApplication.java
+++ sport_event_service/src/main/java/ca/nomosnow/sport_event_service/SportEventServiceApplication.java
@@ -3,6 +3,7 @@ package ca.nomosnow.sport_event_service;
 import ca.nomosnow.sport_event_service.model.SportEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
@@ -14,6 +15,7 @@ import java.util.Locale;
  * Start Standalone SpringBootApplication with SportEventService
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SportEventServiceApplication {
 
     public static void main(String[] args) {
