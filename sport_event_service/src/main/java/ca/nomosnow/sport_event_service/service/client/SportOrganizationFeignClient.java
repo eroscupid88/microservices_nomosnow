a9085eed2261384299b8e7fb1f58c65d90de1edd
Eroscupid <vutuanduong2003@yahoo.com>
2021-09-24 11:45:50 -0600

    add gateway server

---
+++ sport_event_service/src/main/java/ca/nomosnow/sport_event_service/service/client/SportOrganizationFeignClient.java
@@ -0,0 +1,18 @@
+package ca.nomosnow.sport_event_service.service.client;
+
+import ca.nomosnow.sport_event_service.model.SportOrganization;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("sport-organization-service")
+public interface SportOrganizationFeignClient {
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/v1/sportOrganization/{sportOrganizationId}",
+            consumes = "application/json")
+    SportOrganization getSportOrganization(@PathVariable("sportOrganizationId") String organization);
+
+
+}
