35f19310523f9eb4d1b820b3402b6801c79fe162
Eroscupid <vutuanduong2003@yahoo.com>
2021-09-27 17:15:09 -0600

    commit

---
+++ sport-organization-service/src/main/java/ca/nomosnow/sportorganizationservice/model/utils/ResponseWrapper.java
@@ -0,0 +1,31 @@
+package ca.nomosnow.sportorganizationservice.model.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+
+@Getter
+@Setter
+@ToString
+@JsonInclude(NON_NULL)
+public class ResponseWrapper {
+
+    private Object data;
+    private Object metadata;
+    private List<ErrorMessage> errors;
+
+	public ResponseWrapper(Object data, Object metadata, List<ErrorMessage> errors) {
+		super();
+		this.data = data;
+		this.metadata = metadata;
+		this.errors = errors;
+	}
+
+   
+}
