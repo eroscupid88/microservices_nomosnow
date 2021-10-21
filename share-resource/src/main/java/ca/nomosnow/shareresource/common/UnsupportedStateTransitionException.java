251fa542f7e0587da9eefef8009a7f4dca2b58bc
Eroscupid <vutuanduong2003@yahoo.com>
2021-10-21 17:07:33 -0600

    delete some unnecessary files

--- share-resource/src/main/java/ca/nomosnow/shareresource/common/UnsupportedStateTransitionException.java
+++
@@ -1,7 +0,0 @@
-package ca.nomosnow.shareresource.common;
-
-public class UnsupportedStateTransitionException extends RuntimeException {
-  public UnsupportedStateTransitionException(Enum state) {
-    super("current state: " + state);
-  }
-}
--- sport-event-service-api/src/main/java/ca/nomosnow/sporteventserviceapi/events/SportEventDomainEvent.java
+++
@@ -1,6 +0,0 @@
-package ca.nomosnow.sporteventserviceapi.events;
-import io.eventuate.tram.events.common.DomainEvent;
-
-public interface SportEventDomainEvent extends DomainEvent {
-
-}
