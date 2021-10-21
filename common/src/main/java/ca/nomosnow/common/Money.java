26adc06ff9095bf96407da06e4ee16549755ed4a
Eroscupid <vutuanduong2003@yahoo.com>
2021-10-21 16:59:46 -0600

    change in common

--- common/src/main/java/ca/nomosnow/common/Money.java
+++ common/src/main/java/ca/nomosnow/common/Money.java
@@ -4,10 +4,8 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-import javax.persistence.Embeddable;
 import java.math.BigDecimal;
 
-@Embeddable
 //@Access(AccessType.FIELD)
 public class Money {
 
--- event-request-service/src/main/java/ca/nomosnow/eventrequest/domain/model/EventRequest.java
+++ event-request-service/src/main/java/ca/nomosnow/eventrequest/domain/model/EventRequest.java
@@ -1,5 +1,8 @@
 package ca.nomosnow.eventrequest.domain.model;
 
+import ca.nomosnow.common.Address;
+import ca.nomosnow.common.Money;
+import ca.nomosnow.common.Sport;
 import ca.nomosnow.eventrequest.domain.EventRequestState;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,6 +27,14 @@ public class EventRequest {
     private Long comsumerId;
 
     private Long SportOrganizationEventCreateDepartmentId;
+
+    @Embedded
+    private Money money = new Money(Integer.MAX_VALUE);
+    @Embedded
+    private Sport sport;
+    @Embedded
+    private Address address;
+
 
     public EventRequest() {
 
